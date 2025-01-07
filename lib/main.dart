ba64cdfbb5d0083a36978deacdf16045177e8e04
Matthew Drennan <64912127+MattDrennan@users.noreply.github.com>
2025-01-06 22:55:03 -0500

    View Troops

--- lib/main.dart
+++ lib/main.dart
@@ -434,13 +434,19 @@ class MyHomePage extends StatelessWidget {
               width: 200,
             ),
             const SizedBox(height: 20),
-            /*ElevatedButton(
-          onPressed: () {
-            // Navigate to View Troops page
-          },
-          child: const Text('View Troops'),
-        ),
-        const SizedBox(height: 20),*/
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const TroopPage()),
+                  );
+                },
+                child: const Text('View Troops'),
+              ),
+            ),
+            const SizedBox(height: 20),
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
@@ -501,6 +507,163 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
+class TroopPage extends StatefulWidget {
+  const TroopPage({super.key});
+
+  @override
+  State<TroopPage> createState() => _TroopPageState();
+}
+
+class _TroopPageState extends State<TroopPage> {
+  List<dynamic> troops = [];
+  int selectedSquad = 0;
+
+  Future<void> fetchTroops(int squad) async {
+    try {
+      final response = await http.get(
+        Uri.parse(
+            'https://www.fl501st.com/troop-tracker/mobileapi.php?squad=$squad&action=get_troops_by_squad'),
+      );
+
+      selectedSquad = squad;
+
+      if (!mounted) return; // Ensure widget is mounted before proceeding
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        setState(() {
+          troops = data['troops'];
+        });
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Failed to load troops.')),
+        );
+      }
+    } on TimeoutException catch (_) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Request timed out. Please try again.')),
+        );
+      }
+    } catch (e) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error: $e')),
+        );
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    selectedSquad = 0;
+    troops = [];
+    fetchTroops(0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(selectedSquad);
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Troops'),
+        ),
+        body: Column(
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(6, (int indexSquad) {
+                  // Map index to squad name
+                  const squadNames = [
+                    'Florida Garrison',
+                    'Everglades Squad',
+                    'Makaze',
+                    'Parjai',
+                    'Squad 7',
+                    'Tampa'
+                  ];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: ElevatedButton(
+                      onPressed: () => fetchTroops(indexSquad),
+                      child: Text(squadNames[indexSquad]),
+                    ),
+                  );
+                }),
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: List.generate(
+                    troops.length,
+                    (int index) => Padding(
+                      padding: const EdgeInsets.only(
+                          bottom: 16.0), // Adds margin between buttons
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ChatScreen(
+                                    troopName: troops[index]['name'],
+                                    threadId: troops[index]['thread_id'],
+                                    postId: troops[index]['post_id']),
+                              ),
+                            );
+                          },
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 10.0),
+                                child: Image.asset(
+                                  [
+                                    'assets/icons/garrison_icon.png',
+                                    'assets/icons/everglades_icon.png',
+                                    'assets/icons/makaze_icon.png',
+                                    'assets/icons/parjai_icon.png',
+                                    'assets/icons/squad7_icon.png',
+                                    'assets/icons/tampabay_icon.png'
+                                  ][(troops[index]['squad'] ?? 0).clamp(0,
+                                      5)], // Clamp ensures index stays within valid range (0-5)
+                                  width: 24,
+                                  height: 24,
+                                ),
+                              ),
+                              Text(troops[index]['name'] ?? 'Unknown Name'),
+                              SizedBox(height: 5),
+                              Text(
+                                (troops[index]['trooper_count'] ?? 0) ==
+                                        0 // Explicit comparison to 0
+                                    ? 'NOT ENOUGH TROOPERS FOR THIS EVENT!'
+                                    : '${troops[index]['trooper_count']?.toString() ?? '0'} Troopers Attending',
+                                style: TextStyle(
+                                  color:
+                                      (troops[index]['trooper_count'] ?? 0) ==
+                                              0 // Explicit comparison to 0
+                                          ? Colors.red
+                                          : Colors.blue,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
 
@@ -541,7 +704,7 @@ class _ChatPageState extends State<ChatPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Troops'),
+        title: const Text('My Troops'),
       ),
       body: SingleChildScrollView(
         child: Column(
